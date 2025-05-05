b8947c0734831fe20640f66d46e5ab8c7edc3821
Nikita Lysiuk <nikita.lisyuk06@gmail.com>
2025-05-05 23:38:14 +0200

    add first version of User

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using Models;
 
 class Program
 {
     public static void Main()
     {
-
+        User user = User.Create("John Doe");
+        Console.WriteLine($"User created with ID: {user.Id} and Name: {user.Name}");
     }
 }
 
--- models/User.cs
+++ models/User.cs
@@ -1,23 +1,40 @@
-namespace Models {
-    class User {
-        private string id;
-        private string name;
-        
-        private string phone;
+using System.ComponentModel.DataAnnotations;
 
-        private Sex sex;
+namespace Models
+{
+    class User
+    {
+        [Key]
+        public int Id { get; private set; }
 
-        public string GetSex() {
-            if (sex == Sex.THEYTHEM) {
-                return "They/them";
-            } else {
-                throw new Exception("No valid sex");
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; private set; }
+
+        private User(string name)
+        {
+            Name = name;
+        }
+
+        public static User Create(string name)
+        {
+            var user = new User(name);
+            user.Validate();
+            return user;
+        }
+
+        private void Validate()
+        {
+            var context = new ValidationContext(this);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(this, context, results, true);
+
+            if (!isValid)
+            {
+                var errorMessages = string.Join("; ", results.Select(r => r.ErrorMessage));
+                throw new ValidationException($"User validation failed: {errorMessages}");
             }
         }
-    }
-
-
-    enum Sex {
-        THEYTHEM
     }
 }
