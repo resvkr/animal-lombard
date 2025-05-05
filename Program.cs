e0adc5169c5d6e300698e73c29a602d747ffffb1
Nikita Lysiuk <nikita.lisyuk06@gmail.com>
2025-05-05 16:47:55 +0200

    folder structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Piska {
+    public double size;
+}
+
+
+class User  {
+    private string name;
+    Piska pisun = new Piska();
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
+
+
+class Main {
+    public static void main() {
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
